4b819b83beaa77126cd7675e850420bb49fbfcf0
Prostack2003 <cool.deds@mail.ru>
2025-03-15 14:37:13 +0300

    orm: установил doctrine orm и добавил конфигурацию

---
+++ bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require_once "vendor/autoload.php";
+
+$config = ORMSetup::createAttributeMetadataConfiguration(
+    paths: [__DIR__ . '/src/Entity'],
+    isDevMode: true,
+);
+
+
+$connection = DriverManager::getConnection([
+    'driver' => 'pdo_mysql',
+    'dbname' => $_ENV['DB_DATABASE'],
+    'user' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'host' => $_ENV['DB_CONNECTION'],
+], $config);
+
+
+$entityManager = new EntityManager($connection, $config);
+
+global $entityManager;
