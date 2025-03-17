3cdea868cb70ddc101ab71c67c1cdc3370eb2119
Prostack2003 <cool.deds@mail.ru>
2025-03-17 09:39:03 +0300

    laravel-arp: исправил конфиг

---
+++ eloquent.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+require_once "vendor/autoload.php";
+
+
+use Dotenv\Dotenv;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$capsule = new Capsule;
+
+$params = [
+    'host' => $_ENV['DB_HOST'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'database' => $_ENV['DB_DATABASE'],
+    'driver' => 'mysql',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
+
+$capsule->addConnection($params);
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
