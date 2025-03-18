f0b518fb8141461ac7e7d27aea00faf5d5f407b0
Prostack2003 <cool.deds@mail.ru>
2025-03-18 16:42:31 +0300

    curl-refactror: добавил взаимодействие с email api

--- src/Controllers/CurlController.php
+++ src/Controllers/CurlController.php
@@ -10,12 +10,19 @@ class CurlController
     {
         $handle = curl_init();
 
-        $url = 'https://example.com';
+        $url = 'https://api.emailable.com/v1/verify?email=cool.deds@mail.ru&api_key=test_37c5b7a0b7ccff58121a';
 
         curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 
-        curl_exec($handle);
+        $content = curl_exec($handle);
+            if ($content !== false)
+            {
+                $data = json_decode($content, true);
 
-        curl_close($handle);
+                echo "<pre>";
+                print_r($data);
+            }
+
     }
 }
